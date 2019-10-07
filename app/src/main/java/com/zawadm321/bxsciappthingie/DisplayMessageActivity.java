8098ed382087eb5b0f83fdaff332d4232a5b1260
8bitzawad <32941432+8bitzawad@users.noreply.github.com>
2019-10-07 01:40:11 -0400

    Firebase Should be working

--- app/src/main/java/com/zawadm321/bxsciappthingie/DisplayMessageActivity.java
+++ app/src/main/java/com/zawadm321/bxsciappthingie/DisplayMessageActivity.java
@@ -1,7 +1,7 @@
 package com.zawadm321.bxsciappthingie;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
